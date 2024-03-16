8e422efe1bc0c6c02a2d635c66b1f5ef1c3ea368
emran <emrankhanent@gmail.com>
2024-03-16 21:33:23 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeUI(),
-      debugShowCheckedModeBanner: false,
       title: "MyApp",
+      home: HomeUI(),
       theme: ThemeData(primarySwatch: Colors.amber),
       darkTheme: ThemeData(primarySwatch: Colors.brown),
-      color: Colors.amber,
+      debugShowCheckedModeBanner: false,
 
     );
   }
@@ -26,10 +26,7 @@ class HomeUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello"),
-        // backgroundColor: Colors.cyan,
-      ),
+      appBar: AppBar(),
     );
   }
 }
