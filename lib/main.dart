c81830a6ac162bd80347f1fba69fadf28514b5b0
emran <emrankhanent@gmail.com>
2024-03-25 19:15:03 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -11,129 +11,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeUI(),
+    debugShowCheckedModeBanner: false,
     );
   }
 }
 
+
 class HomeUI extends StatelessWidget {
-  HomeUI({super.key});
-
-  var inpItem = [
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra1n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Kha2n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Parve3j"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "M4D"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra5n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra1n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Kha2n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Parve3j"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "M4D"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra5n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra1n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Kha2n"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Parve3j"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "M4D"
-    },
-    {
-      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
-      "title": "Emra5n"
-    },
-  ];
-
-  mySnackbar(message, delay, context) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        backgroundColor: Colors.red,
-        content: Text(message),
-        duration: Duration(seconds: delay),
-        action: SnackBarAction(
-            label: "OK",
-            textColor: Colors.white,
-            backgroundColor: Colors.green,
-            onPressed: () {}),
-      ),
-    );
-  }
+  const HomeUI({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "GridViewBuilder",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 0,
-            crossAxisSpacing: 5,
-            childAspectRatio: 1.3),
-        itemCount: inpItem.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              mySnackbar(inpItem[index]['title'], 2, context);
-            },
-            child: Container(
-              margin: EdgeInsets.all(2),
-              height: 250,
-              width: double.infinity,
-              child: Image.network(
-                inpItem[index]["img"]!,
-                fit: BoxFit.fill,
-              ),
-            ),
-          );
-        },
+        title: Text("Inventory App",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+        backgroundColor: Colors.amber,
+      actions: [
+        IconButton(onPressed: (){}, icon: Icon(Icons.message,color: Colors.white,)),
+        IconButton(onPressed: (){}, icon: Icon(Icons.search,color: Colors.white,)),
+        IconButton(onPressed: (){}, icon: Icon(Icons.settings,color: Colors.white,)),
+        IconButton(onPressed: (){}, icon: Icon(Icons.email,color: Colors.white,)),
+      ],
       ),
     );
   }
 }
+
