85ba106800962c3372d42970ed08fe0878a64fce
emran <emrankhanent@gmail.com>
2024-03-25 19:21:53 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,91 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeUI(),
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-
 class HomeUI extends StatelessWidget {
   const HomeUI({super.key});
+
+
+  mySnackbar(message, delay, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.green,
+        content: Text(message),
+        duration: Duration(seconds: delay),
+        action: SnackBarAction(
+            label: "OK",
+            textColor: Colors.white,
+            backgroundColor: Colors.green,
+            onPressed: () {}),
+      ),
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Inventory App",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+        title: Text(
+          "Inventory App",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         backgroundColor: Colors.amber,
-      actions: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.message,color: Colors.white,)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.search,color: Colors.white,)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.settings,color: Colors.white,)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.email,color: Colors.white,)),
-      ],
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.message,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.email,
+                color: Colors.white,
+              )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add,color: Colors.white,),
+        backgroundColor: Colors.amber,
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "HOME"),
+          BottomNavigationBarItem(icon: Icon(Icons.message),label: "Message"),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
+        ],
+        backgroundColor: Colors.amberAccent,
+        selectedItemColor: Colors.brown,
+        unselectedItemColor: Colors.white,
+
+
+
       ),
     );
   }
 }
-
