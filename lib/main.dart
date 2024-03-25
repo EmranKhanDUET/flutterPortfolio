027f7c6ef3f2b261ac2943e05d8ec3b314118f39
emran <emrankhanent@gmail.com>
2024-03-25 12:06:44 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,7 @@ class HomeUI extends StatelessWidget {
               mySnackbar(inpItem[index]['title'], 2, context);
             },
             child: Container(
-              margin: EdgeInsets.all(0),
+              margin: EdgeInsets.all(2),
               height: 250,
               width: double.infinity,
               child: Image.network(
