1076da216913504a7d799b2146b6ec037747ef71
emran <emrankhanent@gmail.com>
2024-03-25 11:08:24 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -18,57 +18,55 @@ class MyApp extends StatelessWidget {
 class HomeUI extends StatelessWidget {
   HomeUI({super.key});
 
-  ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: Colors.green,
-      foregroundColor: Colors.yellow,
-      padding: EdgeInsets.all(30),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(50)),
-      ));
-
-  mySnackbar(message, delay, context) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        backgroundColor: Colors.green,
-        content: Text(message),
-        duration: Duration(seconds: delay),
-        action: SnackBarAction(
-            label: "OK",
-            textColor: Colors.white,
-            backgroundColor: Colors.red,
-            onPressed: () {}),
-      ),
-    );
-  }
+  ButtonStyle elevatedButton = ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue,
+    foregroundColor: Colors.white,
+    minimumSize: Size(double.infinity, 60),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("Inventory App"),
+        title: Text("Form"),
         centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          TextButton(
-              onPressed: () {
-                mySnackbar("Text Button", 2, context);
-              },
-              child: Text("TEXT BUTTON")),
-          ElevatedButton(
-            onPressed: () {
-              mySnackbar("Elevated Button", 2, context);
-            },
-            child: Text("Elevated  BUTTON"),
-            style: elevatedButtonStyle,
-          ),
-          OutlinedButton(
-              onPressed: () {
-                mySnackbar("Outlined Button", 2, context);
-              },
-              child: Text("Outlined BUTTON")),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "First Name",
+                ),
+              )),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Last Name",
+                ),
+              )),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email Name",
+                ),
+              )),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text("SUBMIT"),
+              style: elevatedButton,
+            ),
+          )
         ],
       ),
     );
