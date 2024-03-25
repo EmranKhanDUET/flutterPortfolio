acd3c4f2aa9dfc4b78b153a1b3a9b48a8d67d9d0
emran <emrankhanent@gmail.com>
2024-03-25 11:40:34 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,77 @@ class MyApp extends StatelessWidget {
 class HomeUI extends StatelessWidget {
   HomeUI({super.key});
 
-  ButtonStyle elevatedButton = ElevatedButton.styleFrom(
-    backgroundColor: Colors.blue,
-    foregroundColor: Colors.white,
-    minimumSize: Size(double.infinity, 60),
-  );
+  var inpItem = [
+    {
+      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
+      "title": "Emra1n"
+    },
+    {
+      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
+      "title": "Kha2n"
+    },
+    {
+      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
+      "title": "Parve3j"
+    },
+    {
+      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
+      "title": "M4D"
+    },
+    {
+      "img": "https://avatars.githubusercontent.com/u/57840119?v=4",
+      "title": "Emra5n"
+    },
+  ];
+
+
+
+  mySnackbar(message, delay, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(message),
+        duration: Duration(seconds: delay),
+        action: SnackBarAction(
+            label: "OK",
+            textColor: Colors.white,
+            backgroundColor: Colors.green,
+            onPressed: () {}),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Form"),
+        title: Text(
+          "ListViewBuilder",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "First Name",
-                ),
-              )),
-          Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Last Name",
-                ),
-              )),
-          Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Email Name",
-                ),
-              )),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("SUBMIT"),
-              style: elevatedButton,
+      body: ListView.builder(
+        itemCount: inpItem.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {mySnackbar(inpItem[index]['title'], 2, context);},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              height: 250,
+              width: double.infinity,
+              child: Image.network(
+                inpItem[index]["img"]!,
+                fit: BoxFit.fill,
+              ),
             ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
