e1f6b7ba9d17f43bb9f72c727a7de44149d5f7a5
emran <emrankhanent@gmail.com>
2024-03-26 12:39:35 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// Modeule7Class1
+// TextFormField and TextField almost same, TextFormField e shudumatro validator namok ekta extra properties ase
+// validator diye text validation kora jay
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,14 +30,46 @@ class HomeUI extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: TextField(
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-          decoration: InputDecoration(
+        child: Padding(
+          padding: EdgeInsets.all(60),
+          child: TextField(
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+            // controller: TextEditingController(),
+            maxLength: 10,
 
+            decoration: InputDecoration(
+              // enabled: false,
+              // amader at least focusedborder and enabledborder define kore dite hobe
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.yellow, width: 5)),
+              enabledBorder:OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.red, width: 5)),
+              disabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.grey, width: 5)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Colors.green, width: 5)),
+              hintText: "Enter Your name",
+              hintStyle: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+              labelText: "NAME",
+              fillColor: Colors.white,
+              filled: true,
+              prefixIcon: Icon(Icons.add),
+              prefixIconColor: Colors.green,
+              suffixIcon: Icon(Icons.remove_red_eye),
+              suffixIconColor: Colors.red,
+            ),
           ),
         ),
       ),
