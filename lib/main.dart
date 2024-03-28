17ff4348c51b41dccc51f72e42086e4540a41a82
emran <emrankhanent@gmail.com>
2024-03-28 11:19:05 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeActivity(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
