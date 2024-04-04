fed7011f546bd5b06438cb961d7bc3096ccebc0b
emran <emrankhanent@gmail.com>
2024-04-05 02:53:56 +0600

    this is an update

---
+++ lib/Activities/HomeActivity.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
+
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  int n = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CounterApp"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          n.toString(),
+          style: const TextStyle(fontSize: 40),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          n++;
+          setState(() {
+
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-// Module8Liveclass2
-
 import 'package:flutter/material.dart';
 
-void main() {
+import 'Activities/HomeActivity.dart';
+
+void main(){
   runApp(MyApp());
 }
 
@@ -12,41 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeUI(),
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyles.elevatedStyle)
-      ),
+      home: HomeActivity(),
     );
   }
-}
-
-class HomeUI extends StatelessWidget {
-  const HomeUI({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink.shade50,
-      appBar: AppBar(
-        title: Text("Theme"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () {}, child: Text("Elevated Button")),
-            TextButton(onPressed: () {}, child: Text("Text Button")),
-            TextField(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonStyles {
-  static ButtonStyle elevatedStyle = ElevatedButton.styleFrom(
-    backgroundColor: Colors.amber,
-    foregroundColor: Colors.white,
-  );
 }
