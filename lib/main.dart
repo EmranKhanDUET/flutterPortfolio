b8c858918911c66ad83dee49cbd282cbc73bdcd9
emran <emrankhanent@gmail.com>
2024-04-05 01:59:35 +0600

    this is an update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,52 @@
 // Module8Liveclass2
 
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeUI(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyles.elevatedStyle)
+      ),
+    );
+  }
+}
+
+class HomeUI extends StatelessWidget {
+  const HomeUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink.shade50,
+      appBar: AppBar(
+        title: Text("Theme"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text("Elevated Button")),
+            TextButton(onPressed: () {}, child: Text("Text Button")),
+            TextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonStyles {
+  static ButtonStyle elevatedStyle = ElevatedButton.styleFrom(
+    backgroundColor: Colors.amber,
+    foregroundColor: Colors.white,
+  );
+}
