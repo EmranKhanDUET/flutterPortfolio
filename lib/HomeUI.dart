6486d73d5854b85dbf9f4eb45090ced6d446948e
emran <emrankhanent@gmail.com>
2024-04-18 07:25:25 +0600

    this is an update

--- lib/HomeUI.dart
+++ lib/HomeUI.dart
@@ -6,25 +6,23 @@ class HomeUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Photo Gallery",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          "Photo Gallery",
+          style: TextStyle(color: Colors.white),
         ),
-        body: Container(
-          height: 300,
-          width: double.infinity,
-          color: Colors.red,
-          alignment: Alignment.center,
-          child: AspectRatio(
-            aspectRatio: 16 / 9,
-            child: Container(
-              color: Colors.green,
-            ),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          heightFactor: .2,
+          widthFactor: .7,
+          child: Container(
+            color: Colors.green,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
